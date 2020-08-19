f6ae75d9800267e90cdea45a6b74170463b8edc1
Yevgeniy Momsikov <e.momsikov@gmail.com>
2020-08-19 11:01:39 +0600

    Adding actual INFO

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,6 +1,22 @@
 public class Variable {
 	public static void main(String[] args) {
 
+		byte processorCore = 4;
+		short systemType = 64;
+		int windowsUpdVerison = 2004;
+		long irqChannel = 4294967267L;
+		float ram = 8.00F;
+		double hdd = 930.27;
+		char favoriteCharacter = 'q';
+		boolean cdRom = false;
 
+		System.out.println(processorCore);
+		System.out.println(systemType);
+		System.out.println(windowsUpdVerison);
+		System.out.println(irqChannel);
+		System.out.println(ram);
+		System.out.println(hdd);
+		System.out.println(favoriteCharacter);
+		System.out.println(cdRom);
 	}
 }

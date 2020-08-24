b6fc47a81473322ebd7e7d676509d1a19c464b3a
Yevgeniy Momsikov <e.momsikov@gmail.com>
2020-08-24 08:41:42 +0600

    To add HW - Cycle

---
+++ Lesson_1/Cycle.java
@@ -0,0 +1,31 @@
+public class Cycle{
+	public static void main(String[] args){
+
+		for(int i = 0; i <= 20; i++){
+			System.out.println(i);
+		}
+
+		System.out.println();
+
+		int j = 6;
+		while(j >= -6){
+			System.out.println(j);
+			j -= 2;
+		}
+
+		System.out.println();
+
+		int k = 10;
+		int sum = 0;
+		do{
+			if(k % 2 == 0){
+				sum += k;
+				k++;
+			}
+			k++;
+		} while(k <= 20);
+
+		System.out.println(sum);
+
+	}
+}

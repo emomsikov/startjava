b95cfb1246364cdb77dc8904b8cd5df9ccf6d457
Yevgeniy Momsikov <e.momsikov@gmail.com>
2020-09-30 22:12:41 +0600

    first attempt

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+Public class Person {
+	String sex = "male";
+	String name = "Elvin";
+	double height = 177.8;
+	double weight = 68.5;
+	int age = 19;
+
+	void walk() {
+
+	}
+
+	void sit() {
+
+	}
+
+	void run() {
+
+	}
+
+	void speak() {
+
+	}
+
+	void learnJava() {
+
+	}
+}

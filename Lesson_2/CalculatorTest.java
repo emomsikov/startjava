32a8e9b30de19be62dc82d00ffcac47e4c84d911
Yevgeniy Momsikov <e.momsikov@gmail.com>
2020-11-04 22:00:55 +0600

    v2

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -6,17 +6,28 @@ class CalculatorTest {
 		Calculator calc = new Calculator();
 
 		System.out.println("Введите первое число: ");
-		int num1 = scan.nextInt();
-		calc.setNum1(num1);
+		calc.setNum1(scan.nextInt());
 
 		System.out.println("Введите знак математической операции: ");
-		char operator = scan.next().charAt(0);
-		calc.setOperator(operator);
+		calc.setOperator(scan.next().charAt(0));
 
 		System.out.println("Введите второе число: ");
-		int num2 = scan.nextInt();
-		calc.setNum2(num2);
+		calc.setNum2(scan.nextInt());
 
-		System.out.println(calc.getResult());
+		System.out.println(calc.calculate());
+
+		boolean check = true;
+
+		while (check) {
+			System.out.println("Хотите продолжить? [yes/no]: ");
+			String userInput = scan.next();
+			if(userInput.equals("yes")) {
+				check = false;
+				break;
+			} else if(userInput.equals("no")) {
+				check = false;
+				break;
+			}
+		}
 	}
 }
